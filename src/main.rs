ce2155251576b246b6c0e8c4d5f7b50fab931321
Vinh Tran <code@tidefield.dev>
2026-07-18 13:29:09 -0400

    Add line information

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 struct Chunk {
+    lines: Vec<u8>,
     code: Vec<u8>,
     constants: Vec<f64>,
 }
@@ -7,7 +8,8 @@ struct Chunk {
 // as long as we have less than 256 opcodes (aka variants)
 #[repr(u8)]
 enum OpCode {
-    Constant, // followed by the index i of the constant (i.e. chunk.constants[i])
+    // followed by the index i of the constant (i.e. chunk.constants[i])
+    Constant,
     Return,
 }
 
@@ -34,8 +36,9 @@ fn add_constant(chunk: &mut Chunk, value: f64) -> usize {
     chunk.constants.len() - 1
 }
 
-fn write_chunk(chunk: &mut Chunk, byte: u8, _line: usize) {
+fn write_chunk(chunk: &mut Chunk, byte: u8, line: usize) {
     chunk.code.push(byte);
+    chunk.lines.push(line as u8);
 }
 
 fn disassemble_chunk(chunk: &Chunk, name: &str) {
@@ -44,10 +47,12 @@ fn disassemble_chunk(chunk: &Chunk, name: &str) {
     let mut offset = 0;
     while offset < chunk.code.len() {
         let byte = chunk.code[offset];
-        print!("{:04} ", offset);
+        let line = chunk.lines[offset];
+        print!("{:04} {:4} ", offset, line);
         match OpCode::try_from(byte) {
             Ok(OpCode::Constant) => {
                 let constant_index = chunk.code[offset + 1] as usize;
+                // NOTE: This will panic when out of bounds
                 let constant_value = chunk.constants[constant_index];
                 println!("OP_CONSTANT {} '{}'", constant_index, constant_value);
                 offset += 2;
@@ -65,16 +70,17 @@ fn disassemble_chunk(chunk: &Chunk, name: &str) {
 
 fn main() {
     let mut chunk = Chunk {
+        lines: Vec::with_capacity(8),
         code: Vec::with_capacity(8),
         constants: Vec::with_capacity(8),
     };
 
     let constant = add_constant(&mut chunk, 1.2);
-    write_chunk(&mut chunk, OpCode::Constant as u8, constant);
+    write_chunk(&mut chunk, OpCode::Constant as u8, 123);
     // TODO: this will panic when constant is greater than 255
-    write_chunk(&mut chunk, constant as u8, constant);
+    write_chunk(&mut chunk, constant as u8, 123);
 
-    write_chunk(&mut chunk, OpCode::Return as u8, constant);
+    write_chunk(&mut chunk, OpCode::Return as u8, 123);
 
     disassemble_chunk(&chunk, "test chunk");
 }
